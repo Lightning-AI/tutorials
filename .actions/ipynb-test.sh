f0ab401d1dee8ea8f55ac598b7ef9902fe0bd9e5
Jirka <jirka.borovec@seznam.cz>
2021-05-27 18:15:52 +0200

    local

--- .actions/ipynb-test.sh
+++ .actions/ipynb-test.sh
@@ -7,13 +7,15 @@ python -c "import glob ; assert(len(glob.glob('$1/*.ipynb')) == 1)"
 ipynb_file=( $(ls "$1"/*.ipynb) )
 echo $ipynb_file
 
-python .actions/helpers.py parse-requirements $1
 pip install --quiet --requirement requirements.txt
+
+python .actions/helpers.py parse-requirements $1
+cat "$1/requirements.txt"
 
 python -m venv --system-site-packages "$1/venv"
 source "$1/venv/bin/activate"
 pip install --requirement "$1/requirements.txt"
 
-treon -v $ipynb_file
+python -m treon -v $ipynb_file
 
 deactivate
