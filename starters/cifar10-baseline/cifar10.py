23dedcfa0cf9a854f5ad3356d810d2d66ae008c8
Jirka <jirka.borovec@seznam.cz>
2021-05-29 09:54:19 +0200

    add scripts

---
+++ starters/cifar10-baseline/cifar10.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     name: python3
+# ---
+
+# %% [markdown] id="qMDj0BYNECU8"
+# <a href="https://colab.research.google.com/github/PytorchLightning/pytorch-lightning/blob/master/notebooks/06-cifar10-pytorch-lightning.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# %% [markdown] id="ECu0zDh8UXU8"
+# # PyTorch Lightning CIFAR10 ~94% Baseline Tutorial ⚡
+#
+# Train a Resnet to 94% accuracy on Cifar10!
+#
+# Main takeaways:
+# 1. Experiment with different Learning Rate schedules and frequencies in the configure_optimizers method in pl.LightningModule
+# 2. Use an existing Resnet architecture with modifications directly with Lightning
+#
+# ---
+#
+#   - Give us a ⭐ [on Github](https://www.github.com/PytorchLightning/pytorch-lightning/)
+#   - Check out [the documentation](https://pytorch-lightning.readthedocs.io/en/latest/)
+#   - Join us [on Slack](https://join.slack.com/t/pytorch-lightning/shared_invite/zt-pw5v393p-qRaDgEk24~EjiZNBpSQFgQ)
+
+# %% [markdown] id="HYpMlx7apuHq"
+# ### Setup
+# Lightning is easy to install. Simply `pip install pytorch-lightning`.
+# Also check out [bolts](https://github.com/PyTorchLightning/lightning-bolts/) for pre-existing data modules and models.
+
+# %% id="ziAQCrE-TYWG"
+# ! pip install pytorch-lightning lightning-bolts -qU
+
+# %% id="L-W_Gq2FORoU"
+# Run this if you intend to use TPUs
+# # !curl https://raw.githubusercontent.com/pytorch/xla/master/contrib/scripts/env-setup.py -o pytorch-xla-env-setup.py
+# # !python pytorch-xla-env-setup.py --version nightly --apt-packages libomp5 libopenblas-dev
+
+# %% id="wjov-2N_TgeS"
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+import torchvision
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.metrics.functional import accuracy
+from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+
+# %% id="54JMU1N-0y0g"
+pl.seed_everything(7)
+
+# %% [markdown] id="FA90qwFcqIXR"
+# ### CIFAR10 Data Module
+#
+# Import the existing data module from `bolts` and modify the train and test transforms.
+
+# %% id="S9e-W8CSa8nH"
+batch_size = 32
+
+train_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.RandomCrop(32, padding=4),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    cifar10_normalization(),
+])
+
+test_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    cifar10_normalization(),
+])
+
+cifar10_dm = CIFAR10DataModule(
+    batch_size=batch_size,
+    train_transforms=train_transforms,
+    test_transforms=test_transforms,
+    val_transforms=test_transforms,
+)
+
+# %% [markdown] id="SfCsutp3qUMc"
+# ### Resnet
+# Modify the pre-existing Resnet architecture from TorchVision. The pre-existing architecture is based on ImageNet images (224x224) as input. So we need to modify it for CIFAR10 images (32x32).
+
+
+# %% id="GNSeJgwvhHp-"
+def create_model():
+    model = torchvision.models.resnet18(pretrained=False, num_classes=10)
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+    model.maxpool = nn.Identity()
+    return model
+
+
+# %% [markdown] id="HUCj5TKsqty1"
+# ### Lightning Module
+# Check out the [`configure_optimizers`](https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#configure-optimizers) method to use custom Learning Rate schedulers. The OneCycleLR with SGD will get you to around 92-93% accuracy in 20-30 epochs and 93-94% accuracy in 40-50 epochs. Feel free to experiment with different LR schedules from https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate
+
+
+# %% id="03OMrBa5iGtT"
+class LitResnet(pl.LightningModule):
+
+    def __init__(self, lr=0.05):
+        super().__init__()
+
+        self.save_hyperparameters()
+        self.model = create_model()
+
+    def forward(self, x):
+        out = self.model(x)
+        return F.log_softmax(out, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = F.log_softmax(self.model(x), dim=1)
+        loss = F.nll_loss(logits, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+
+        if stage:
+            self.log(f'{stage}_loss', loss, prog_bar=True)
+            self.log(f'{stage}_acc', acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, 'val')
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, 'test')
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.hparams.lr, momentum=0.9, weight_decay=5e-4)
+        steps_per_epoch = 45000 // batch_size
+        scheduler_dict = {
+            'scheduler': OneCycleLR(optimizer, 0.1, epochs=self.trainer.max_epochs, steps_per_epoch=steps_per_epoch),
+            'interval': 'step',
+        }
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler_dict}
+
+
+# %% id="3FFPgpAFi9KU"
+model = LitResnet(lr=0.05)
+model.datamodule = cifar10_dm
+
+trainer = pl.Trainer(
+    progress_bar_refresh_rate=20,
+    max_epochs=40,
+    gpus=1,
+    logger=pl.loggers.TensorBoardLogger('lightning_logs/', name='resnet'),
+    callbacks=[LearningRateMonitor(logging_interval='step')],
+)
+
+trainer.fit(model, cifar10_dm)
+trainer.test(model, datamodule=cifar10_dm)
+
+# %% [markdown] id="lWL_WpeVIXWQ"
+# ### Bonus: Use [Stochastic Weight Averaging](https://arxiv.org/abs/1803.05407) to get a boost on performance
+#
+# Use SWA from torch.optim to get a quick performance boost. Also shows a couple of cool features from Lightning:
+# - Use `training_epoch_end` to run code after the end of every epoch
+# - Use a pretrained model directly with this wrapper for SWA
+
+
+# %% id="bsSwqKv0t9uY"
+class SWAResnet(LitResnet):
+
+    def __init__(self, trained_model, lr=0.01):
+        super().__init__()
+
+        self.save_hyperparameters('lr')
+        self.model = trained_model
+        self.swa_model = AveragedModel(self.model)
+
+    def forward(self, x):
+        out = self.swa_model(x)
+        return F.log_softmax(out, dim=1)
+
+    def training_epoch_end(self, training_step_outputs):
+        self.swa_model.update_parameters(self.model)
+
+    def validation_step(self, batch, batch_idx, stage=None):
+        x, y = batch
+        logits = F.log_softmax(self.model(x), dim=1)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+
+        self.log(f'val_loss', loss, prog_bar=True)
+        self.log(f'val_acc', acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.hparams.lr, momentum=0.9, weight_decay=5e-4)
+        return optimizer
+
+    def on_train_end(self):
+        update_bn(self.datamodule.train_dataloader(), self.swa_model, device=self.device)
+
+
+# %% id="cA6ZG7C74rjL"
+swa_model = SWAResnet(model.model, lr=0.01)
+swa_model.datamodule = cifar10_dm
+
+swa_trainer = pl.Trainer(
+    progress_bar_refresh_rate=20,
+    max_epochs=20,
+    gpus=1,
+    logger=pl.loggers.TensorBoardLogger('lightning_logs/', name='swa_resnet'),
+)
+
+swa_trainer.fit(swa_model, cifar10_dm)
+swa_trainer.test(swa_model, datamodule=cifar10_dm)
+
+# %% id="RRHMfGiDpZ2M"
+# Start tensorboard.
+# %reload_ext tensorboard
+# %tensorboard --logdir lightning_logs/
+
+# %% [markdown] id="RltpFGS-s0M1"
+# <code style="color:#792ee5;">
+#     <h1> <strong> Congratulations - Time to Join the Community! </strong>  </h1>
+# </code>
+#
+# Congratulations on completing this notebook tutorial! If you enjoyed this and would like to join the Lightning movement, you can do so in the following ways!
+#
+# ### Star [Lightning](https://github.com/PyTorchLightning/pytorch-lightning) on GitHub
+# The easiest way to help our community is just by starring the GitHub repos! This helps raise awareness of the cool tools we're building.
+#
+# * Please, star [Lightning](https://github.com/PyTorchLightning/pytorch-lightning)
+#
+# ### Join our [Slack](https://join.slack.com/t/pytorch-lightning/shared_invite/zt-pw5v393p-qRaDgEk24~EjiZNBpSQFgQ)!
+# The best way to keep up to date on the latest advancements is to join our community! Make sure to introduce yourself and share your interests in `#general` channel
+#
+# ### Interested by SOTA AI models ! Check out [Bolt](https://github.com/PyTorchLightning/lightning-bolts)
+# Bolts has a collection of state-of-the-art models, all implemented in [Lightning](https://github.com/PyTorchLightning/pytorch-lightning) and can be easily integrated within your own projects.
+#
+# * Please, star [Bolt](https://github.com/PyTorchLightning/lightning-bolts)
+#
+# ### Contributions !
+# The best way to contribute to our community is to become a code contributor! At any time you can go to [Lightning](https://github.com/PyTorchLightning/pytorch-lightning) or [Bolt](https://github.com/PyTorchLightning/lightning-bolts) GitHub Issues page and filter for "good first issue".
+#
+# * [Lightning good first issue](https://github.com/PyTorchLightning/pytorch-lightning/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22)
+# * [Bolt good first issue](https://github.com/PyTorchLightning/lightning-bolts/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22)
+# * You can also contribute your own notebooks with useful examples !
+#
+# ### Great thanks from the entire Pytorch Lightning Team for your interest !
+#
+# <img src="https://github.com/PyTorchLightning/pytorch-lightning/blob/master/docs/source/_static/images/logo.png?raw=true" width="800" height="200" />
