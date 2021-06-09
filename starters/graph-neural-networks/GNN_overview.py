44238787daace9fbc03373c5300ca4fbc35f9d5a
Jirka Borovec <Borda@users.noreply.github.com>
2021-06-10 00:05:39 +0200

    jupytext meta

--- starters/graph-neural-networks/GNN_overview.py
+++ starters/graph-neural-networks/GNN_overview.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # In this tutorial, we will discuss the application of neural networks on graphs.
 # Graph Neural Networks (GNNs) have recently gained increasing popularity in both applications and research, including domains such as social networks, knowledge graphs, recommender systems, and bioinformatics.
