c9d493e528ae0224b5d3b1a8e23abddf814c9071
Jirka <jirka.borovec@seznam.cz>
2021-06-17 11:38:32 +0200

    5

--- lightning_examples/trainer-flags-gpu/flags.py
+++ lightning_examples/trainer-flags-gpu/flags.py
@@ -31,7 +31,7 @@ PATH_DATASETS = os.environ.get('PATH_DATASETS', '.')
 AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 256 if AVAIL_GPUS else 64
 NUM_WORKERS = int(os.cpu_count() / 2)
-MAX_EPOCHS = 10
+MAX_EPOCHS = 5
 
 # Init DataLoader from MNIST Dataset
 
