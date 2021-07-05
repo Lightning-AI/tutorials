db642d3d55ce31786b2e94fb9cb4ba451c47c247
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-05 05:00:39 +0200

    x

--- lightning_examples/distributed-training/sdp_example_1.py
+++ lightning_examples/distributed-training/sdp_example_1.py
@@ -90,22 +90,22 @@ if __name__ == "__main__":
     model = TutorialModule()
     datamodule = MNISTDataModule()
 
-    trainer = Trainer(
-        gpus=2,
-        accelerator="ddp",
-        max_epochs=1,
-    )
-    trainer.fit(model, datamodule=datamodule)
-
-    ddp_max_mem = torch.cuda.max_memory_allocated(trainer.local_rank) / 1000
-    print(f"GPU {trainer.local_rank} max memory using DDP: {ddp_max_mem:.2f} MB")
-
     # trainer = Trainer(
     #     gpus=2,
-    #     accelerator="ddp_sharded",
+    #     accelerator="ddp",
     #     max_epochs=1,
     # )
     # trainer.fit(model, datamodule=datamodule)
     #
-    # sdp_max_mem = torch.cuda.max_memory_allocated(trainer.local_rank) / 1000
-    # print(f"GPU {trainer.local_rank} max memory using SDP: {sdp_max_mem:.2f} MB")
+    # ddp_max_mem = torch.cuda.max_memory_allocated(trainer.local_rank) / 1000
+    # print(f"GPU {trainer.local_rank} max memory using DDP: {ddp_max_mem:.2f} MB")
+
+    trainer = Trainer(
+        gpus=2,
+        accelerator="ddp_sharded",
+        max_epochs=1,
+    )
+    trainer.fit(model, datamodule=datamodule)
+
+    sdp_max_mem = torch.cuda.max_memory_allocated(trainer.local_rank) / 1000
+    print(f"GPU {trainer.local_rank} max memory using SDP: {sdp_max_mem:.2f} MB")
