b1bf40e92fa8d81feab9475285c1ae77f24cd367
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-05 05:21:49 +0200

    add prepare notebook script

---
+++ lightning_examples/distributed-training/prepare_notebook.py
@@ -0,0 +1,7 @@
+# patch a few functions that would throw an error when running in the notebook
+# this is because most multi-GPU plugins are only supported in script-mode
+# the user is advised to read the instructions how to run the code samples given in the notebooks
+import pytorch_lightning as pl
+from pytorch_lightning.trainer.connectors.accelerator_connector import AcceleratorConnector
+
+AcceleratorConnector.check_interactive_compatibility = lambda x: True
