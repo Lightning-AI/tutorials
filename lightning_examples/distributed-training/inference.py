dc9de4d8b8a5ca57bee6148037c82af46e0898d6
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-07 16:02:13 +0200

    x

--- lightning_examples/distributed-training/inference.py
+++ lightning_examples/distributed-training/inference.py
@@ -111,6 +111,7 @@ class CustomWriter(BasePredictionWriter):
     def __init__(self, output_dir: str, write_interval: str):
         super().__init__(write_interval)
         self.output_dir = output_dir
+        os.makedirs(output_dir, exist_ok=True)
 
     def write_on_batch_end(
         self, trainer, pl_module, prediction, batch_indices, batch, batch_idx, dataloader_idx
