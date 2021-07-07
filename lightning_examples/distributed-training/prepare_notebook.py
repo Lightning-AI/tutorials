a13aa749e0478151bf8eb538d791595884873909
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-07 10:38:39 -0400

    update inference

--- lightning_examples/distributed-training/prepare_notebook.py
+++ lightning_examples/distributed-training/prepare_notebook.py
@@ -1,6 +1,8 @@
 # patch a few functions that would throw an error when running in the notebook
 # this is because most multi-GPU plugins are only supported in script-mode
 # the user is advised to read the instructions how to run the code samples given in the notebooks
+
+import torch
 from pytorch_lightning.trainer.connectors.accelerator_connector import AcceleratorConnector
 
 AcceleratorConnector.check_interactive_compatibility = lambda x: True
