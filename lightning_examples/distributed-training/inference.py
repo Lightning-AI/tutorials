3cc86516c9379b977b45523b79fb0fd332930d96
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-07-07 15:30:54 +0200

    x

--- lightning_examples/distributed-training/inference.py
+++ lightning_examples/distributed-training/inference.py
@@ -95,11 +95,12 @@ class DDPInferenceModel(TutorialModule):
 class DDPInferenceDataModule(MNISTDataModule):
 
     def setup(self, stage=None):
-        if stage in ('test', 'predict') or stage is None:
-            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+        super().setup(stage=stage)
+        if stage == 'predict' or stage is None:
+            self.mnist_predict = MNIST(self.data_dir, train=False, transform=self.transform)
 
     def predict_dataloader(self):
-        return self.test_dataloader()
+        return DataLoader(self.mnist_predict, batch_size=self.batch_size)
 
 
 def run_train():
