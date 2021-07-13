7ad39b7ec582ef3a6a89c483f3bc21961bd48338
Jirka <jirka.borovec@seznam.cz>
2021-07-13 16:47:40 +0200

    fix folders

--- .actions/helpers.py
+++ .actions/helpers.py
@@ -6,6 +6,7 @@ import shutil
 from datetime import datetime
 from pprint import pprint
 from typing import Sequence
+from warnings import warn
 
 import fire
 import requests
@@ -249,9 +250,19 @@ class HelperCLI:
         dirs_exist = [d for d in dirs if os.path.isdir(d)]
         dirs_invalid = [d for d in dirs_exist if not HelperCLI._meta_file(d)]
         if strict and dirs_invalid:
-            raise FileNotFoundError(
-                f"Following folders do not have valid `{HelperCLI.META_FILE_REGEX}` \n {os.linesep.join(dirs_invalid)}"
+            warn(
+                f"Following folders do not have valid `{HelperCLI.META_FILE_REGEX}`:"
+                f" \n {os.linesep.join(dirs_invalid)}"
             )
+            # check if there is other valid folder in its tree
+            dirs_invalid = [
+                pd for pd in dirs_invalid if not any(p.startswith(pd + os.path.sep) for p in dirs_exist)
+            ]
+            if dirs_invalid:
+                raise FileNotFoundError(
+                    f"Following folders do not have valid `{HelperCLI.META_FILE_REGEX}` nor sub-folder:"
+                    f" \n {os.linesep.join(dirs_invalid)}"
+                )
 
         dirs_change = [d for d in dirs_exist if HelperCLI._meta_file(d)]
         with open(fpath_change_folders, "w") as fp:
