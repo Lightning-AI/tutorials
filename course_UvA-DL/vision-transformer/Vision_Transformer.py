364900321ab017ce8201af9b7c9cf960a898d963
phlippe <phillip.lippe@googlemail.com>
2021-08-21 12:17:12 +0200

    Additional formatting

--- course_UvA-DL/vision-transformer/Vision_Transformer.py
+++ course_UvA-DL/vision-transformer/Vision_Transformer.py
@@ -17,24 +17,25 @@
 # Let's start with importing our standard set of libraries.
 
 # %%
-from urllib.error import HTTPError
+import os
 import urllib.request
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from urllib.error import HTTPError
+
+import matplotlib
+import matplotlib.pyplot as plt
 import pytorch_lightning as pl
-from torchvision import transforms
-from torchvision.datasets import CIFAR10
-import torchvision
+import seaborn as sns
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as data
-import torch.nn.functional as F
-import torch.nn as nn
-import torch
-import seaborn as sns
-import matplotlib
+import torchvision
 from IPython.display import set_matplotlib_formats
-import os
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
 
-import matplotlib.pyplot as plt
 plt.set_cmap('cividis')
 # %matplotlib inline
 set_matplotlib_formats('svg', 'pdf')  # For export
@@ -67,8 +68,10 @@ print("Device:", device)
 # Github URL where saved models are stored for this tutorial
 base_url = "https://raw.githubusercontent.com/phlippe/saved_models/main/"
 # Files to download
-pretrained_files = ["tutorial15/ViT.ckpt", "tutorial15/tensorboards/ViT/events.out.tfevents.ViT",
-                    "tutorial5/tensorboards/ResNet/events.out.tfevents.resnet"]
+pretrained_files = [
+    "tutorial15/ViT.ckpt", "tutorial15/tensorboards/ViT/events.out.tfevents.ViT",
+    "tutorial5/tensorboards/ResNet/events.out.tfevents.resnet"
+]
 # Create checkpoint path if it doesn't exist yet
 os.makedirs(CHECKPOINT_PATH, exist_ok=True)
 
@@ -83,7 +86,10 @@ for file_name in pretrained_files:
         try:
             urllib.request.urlretrieve(file_url, file_path)
         except HTTPError as e:
-            print("Something went wrong. Please try to download the file from the GDrive folder, or contact the author with the full output including the following error:\n", e)
+            print(
+                "Something went wrong. Please try to download the file from the GDrive folder, or contact the author with the full output including the following error:\n",
+                e
+            )
 
 # %% [markdown]
 # We load the CIFAR10 dataset below.
@@ -93,17 +99,17 @@ for file_name in pretrained_files:
 # one.
 
 # %%
-test_transform = transforms.Compose([transforms.ToTensor(),
-                                     transforms.Normalize([0.49139968, 0.48215841, 0.44653091],
-                                                          [0.24703223, 0.24348513, 0.26158784])
-                                     ])
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784])
+])
 # For training, we add some augmentation. Networks are too powerful and would overfit.
-train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                      transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.49139968, 0.48215841, 0.44653091],
-                                                           [0.24703223, 0.24348513, 0.26158784])
-                                      ])
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784])
+])
 # Loading the training dataset. We need to split it into a training and validation part
 # We need to do a little trick because the validation set should not use the augmentation.
 train_dataset = CIFAR10(root=DATASET_PATH, train=True, transform=train_transform, download=True)
@@ -117,7 +123,9 @@ _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
 test_set = CIFAR10(root=DATASET_PATH, train=False, transform=test_transform, download=True)
 
 # We define a set of data loaders that we can use for various purposes later.
-train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4)
+train_loader = data.DataLoader(
+    train_set, batch_size=128, shuffle=True, drop_last=True, pin_memory=True, num_workers=4
+)
 val_loader = data.DataLoader(val_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
 test_loader = data.DataLoader(test_set, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
 
@@ -133,7 +141,6 @@ plt.imshow(img_grid)
 plt.axis('off')
 plt.show()
 plt.close()
-
 
 # %% [markdown]
 # ## Transformers for image classification
@@ -154,6 +161,7 @@ plt.close()
 # First, let's implement the image preprocessing: an image of size $N\times N$ has to be split into $(N/M)^2$ patches of size $M\times M$.
 # These represent the input words to the Transformer.
 
+
 # %%
 def img_to_patch(x, patch_size, flatten_channels=True):
     """
@@ -166,9 +174,9 @@ def img_to_patch(x, patch_size, flatten_channels=True):
     B, C, H, W = x.shape
     x = x.reshape(B, C, H // patch_size, patch_size, W // patch_size, patch_size)
     x = x.permute(0, 2, 4, 1, 3, 5)  # [B, H', W', C, p_H, p_W]
-    x = x.flatten(1, 2)              # [B, H'*W', C, p_H, p_W]
+    x = x.flatten(1, 2)  # [B, H'*W', C, p_H, p_W]
     if flatten_channels:
-        x = x.flatten(2, 4)          # [B, H'*W', C*p_H*p_W]
+        x = x.flatten(2, 4)  # [B, H'*W', C*p_H*p_W]
     return x
 
 
@@ -191,7 +199,6 @@ for i in range(CIFAR_images.shape[0]):
 plt.show()
 plt.close()
 
-
 # %% [markdown]
 # Compared to the original images, it is much harder to recognize the objects from those patch lists now.
 # Still, this is the input we provide to the Transformer for classifying the images.
@@ -208,6 +215,7 @@ plt.close()
 # <center width="100%"><img src="pre_layer_norm.svg" width="400px"></center>
 #
 # The implementation of the Pre-LN attention block looks as follows:
+
 
 # %%
 class AttentionBlock(nn.Module):
@@ -227,11 +235,8 @@ class AttentionBlock(nn.Module):
         self.attn = nn.MultiheadAttention(embed_dim, num_heads)
         self.layer_norm_2 = nn.LayerNorm(embed_dim)
         self.linear = nn.Sequential(
-            nn.Linear(embed_dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, embed_dim),
-            nn.Dropout(dropout)
+            nn.Linear(embed_dim, hidden_dim), nn.GELU(), nn.Dropout(dropout),
+            nn.Linear(hidden_dim, embed_dim), nn.Dropout(dropout)
         )
 
     def forward(self, x):
@@ -257,11 +262,22 @@ class AttentionBlock(nn.Module):
 #
 # With those components in mind, let's implement the full Vision Transformer below:
 
+
 # %%
 class VisionTransformer(nn.Module):
 
-    def __init__(self, embed_dim, hidden_dim, num_channels, num_heads,
-                 num_layers, num_classes, patch_size, num_patches, dropout=0.0):
+    def __init__(
+        self,
+        embed_dim,
+        hidden_dim,
+        num_channels,
+        num_heads,
+        num_layers,
+        num_classes,
+        patch_size,
+        num_patches,
+        dropout=0.0
+    ):
         """
         Inputs:
             embed_dim - Dimensionality of the input feature vectors to the Transformer
@@ -283,11 +299,9 @@ class VisionTransformer(nn.Module):
         # Layers/Networks
         self.input_layer = nn.Linear(num_channels * (patch_size**2), embed_dim)
         self.transformer = nn.Sequential(
-            *[AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)])
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(embed_dim),
-            nn.Linear(embed_dim, num_classes)
+            *[AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)]
         )
+        self.mlp_head = nn.Sequential(nn.LayerNorm(embed_dim), nn.Linear(embed_dim, num_classes))
         self.dropout = nn.Dropout(dropout)
 
         # Parameters/Embeddings
@@ -321,6 +335,7 @@ class VisionTransformer(nn.Module):
 # We use `torch.optim.AdamW` as the optimizer, which is Adam with a corrected weight decay implementation.
 # Since we use the Pre-LN Transformer version, we do not need to use a learning rate warmup stage anymore.
 # Instead, we use the same learning rate scheduler as the CNNs in our previous tutorial on image classification.
+
 
 # %%
 class ViT(pl.LightningModule):
@@ -369,15 +384,20 @@ class ViT(pl.LightningModule):
 # Let's first create a training function for our PyTorch Lightning module
 # which also loads the pre-trained model if you have downloaded it above.
 
+
 # %%
 def train_model(**kwargs):
-    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, "ViT"),
-                         gpus=1 if str(device) == "cuda:0" else 0,
-                         max_epochs=180,
-                         callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
-                                    LearningRateMonitor("epoch")],
-                         progress_bar_refresh_rate=1)
-    trainer.logger._log_graph = True         # If True, we plot the computation graph in tensorboard
+    trainer = pl.Trainer(
+        default_root_dir=os.path.join(CHECKPOINT_PATH, "ViT"),
+        gpus=1 if str(device) == "cuda:0" else 0,
+        max_epochs=180,
+        callbacks=[
+            ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
+            LearningRateMonitor("epoch")
+        ],
+        progress_bar_refresh_rate=1
+    )
+    trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
     trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
 
     # Check whether pretrained model exists. If yes, load it and skip training
@@ -433,18 +453,20 @@ def train_model(**kwargs):
 # the hyperparameter choices on the CIFAR10 dataset.
 
 # %%
-model, results = train_model(model_kwargs={
-    'embed_dim': 256,
-    'hidden_dim': 512,
-    'num_heads': 8,
-    'num_layers': 6,
-    'patch_size': 4,
-    'num_channels': 3,
-    'num_patches': 64,
-    'num_classes': 10,
-    'dropout': 0.2
-},
-    lr=3e-4)
+model, results = train_model(
+    model_kwargs={
+        'embed_dim': 256,
+        'hidden_dim': 512,
+        'num_heads': 8,
+        'num_layers': 6,
+        'patch_size': 4,
+        'num_channels': 3,
+        'num_patches': 64,
+        'num_classes': 10,
+        'dropout': 0.2
+    },
+    lr=3e-4
+)
 print("ViT results", results)
 
 # %% [markdown]
