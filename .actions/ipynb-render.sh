2921faf7f6e32c5bf1c414f8d62e827bbfac690a
Ethan Harris <ethanwharris@gmail.com>
2021-09-09 10:48:00 +0100

    Try something

--- .actions/ipynb-render.sh
+++ .actions/ipynb-render.sh
@@ -5,7 +5,7 @@ printf "Rendering: $1\n\n"
 
 python -c "import os, glob ; assert(len(glob.glob(os.path.join('$1', '*.ipynb'))) == 1)"
 ipynb_file=( $(ls "$1"/*.ipynb) )
-printf "$ipynb_file"
+printf -- $ipynb_file
 
 python -c "import os ; assert any(os.path.isfile(os.path.join('$1', f'.meta{ext}')) for ext in ['.yml', '.yaml'])"
 meta_file=( $(ls "$1"/.meta.*) )
