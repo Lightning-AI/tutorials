e4ae95a0ad77185a7b85c29914c1a1dca0aeef01
Ethan Harris <ethanwharris@gmail.com>
2021-09-16 12:58:40 +0100

    Add a wrapper to video iframes for custom styling

--- course_UvA-DL/deep-energy-based-generative-models/Deep_Energy_Models.py
+++ course_UvA-DL/deep-energy-based-generative-models/Deep_Energy_Models.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# <iframe width="560" height="315" src="https://www.youtube.com/embed/E6PDwquBBQc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+# <div class="video-wrapper"><iframe src="https://www.youtube.com/embed/E6PDwquBBQc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 # First, let's import our standard libraries below.
 
 # %%
@@ -221,7 +221,7 @@ for file_name in pretrained_files:
 # %% [markdown]
 # ## Image generation
 #
-# <iframe width="560" height="315" src="https://www.youtube.com/embed/QJ94zuSQoP4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+# <div class="video-wrapper"><iframe src="https://www.youtube.com/embed/QJ94zuSQoP4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
 #
 # As an example for energy-based models, we will train a model on image generation.
 # Specifically, we will look at how we can generate MNIST digits with a very simple CNN model.
