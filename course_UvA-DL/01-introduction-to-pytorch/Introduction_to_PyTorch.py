0a583d70362e5ada6d7befa01f7b335b77100982
phlippe <phillip.lippe@googlemail.com>
2021-11-01 12:58:47 +0100

    GPU timing optional

--- course_UvA-DL/01-introduction-to-pytorch/Introduction_to_PyTorch.py
+++ course_UvA-DL/01-introduction-to-pytorch/Introduction_to_PyTorch.py
@@ -423,15 +423,16 @@ end_time = time.time()
 print(f"CPU time: {(end_time - start_time):6.5f}s")
 
 # GPU version
-x = x.to(device)
-# CUDA is asynchronous, so we need to use different timing functions
-start = torch.cuda.Event(enable_timing=True)
-end = torch.cuda.Event(enable_timing=True)
-start.record()
-_ = torch.matmul(x, x)
-end.record()
-torch.cuda.synchronize()  # Waits for everything to finish running on the GPU
-print(f"GPU time: {0.001 * start.elapsed_time(end):6.5f}s")  # Milliseconds to seconds
+if torch.cuda.is_available():
+    x = x.to(device)
+    # CUDA is asynchronous, so we need to use different timing functions
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    _ = torch.matmul(x, x)
+    end.record()
+    torch.cuda.synchronize()  # Waits for everything to finish running on the GPU
+    print(f"GPU time: {0.001 * start.elapsed_time(end):6.5f}s")  # Milliseconds to seconds
 
 # %% [markdown]
 # Depending on the size of the operation and the CPU/GPU in your system, the speedup of this operation can be >50x.
