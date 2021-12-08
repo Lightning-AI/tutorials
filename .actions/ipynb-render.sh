5dd7b1c261a3fb6bd5788a9f13f7e95d3ede4987
Jirka <jirka.borovec@seznam.cz>
2021-12-08 13:36:25 +0100

    document process

--- .actions/ipynb-render.sh
+++ .actions/ipynb-render.sh
@@ -3,14 +3,17 @@
 set -e
 printf "Rendering: $1\n\n"
 
+# check that there is no ipython file in the folder
 python -c "import os, glob ; assert(len(glob.glob(os.path.join('$1', '*.ipynb'))) == 1)"
 ipynb_file=( $(ls "$1"/*.ipynb) )
 printf $ipynb_file
 
-python -c "import os ; assert any(os.path.isfile(os.path.join('$1', f'.meta{ext}')) for ext in ['.yml', '.yaml'])"
+# check that there i exactly one meta recipe
+python -c "import os ; assert sum(os.path.isfile(os.path.join('$1', f'.meta{ext}')) for ext in ['.yml', '.yaml']) == 1"
 meta_file=( $(ls "$1"/.meta.*) )
 printf $meta_file
 
+# check that folder has at most one thumb file
 python -c "import os, glob ; assert(len(glob.glob(os.path.join('$1', '.thumb.*'))) <= 1)"
 thumb_file=( $(ls "$1"/.thumb.* 2>/dev/null || echo "") ) || true
 [ ! -z $thumb_file ] && printf $thumb_file
@@ -21,6 +24,7 @@ printf $pub_file
 pub_meta_file=".notebooks/$1.yaml"
 printf $pub_meta_file
 
+# if a notebook has thumb image gets its path
 if [ ! -z $thumb_file ]; then
   pub_thumb_file=".notebooks/"${thumb_file/"$1"\/.thumb/$1}
   printf $pub_thumb_file
@@ -29,18 +33,22 @@ fi
 pub_dir=$(dirname "$pub_file")
 mkdir -p $pub_dir
 
-python .actions/helpers.py parse-requirements $1
+# just in case reinstall the global
 pip install --quiet --requirement requirements.txt --upgrade-strategy only-if-needed
+# prepare the requirements specific for the particular notebook
+python .actions/assistant.py parse-requirements $1
 cat "$1/requirements.txt"
 pip_args=$(cat "$1/pip_arguments.txt")
 printf "pip arguments:\n $pip_args\n\n"
+# and install specific packages
 pip install --requirement "$1/requirements.txt" $pip_args
 
+# dry run does not execute the notebooks just takes them as they are
 if [ ! -z "${DRY_RUN}" ] && [ "${DRY_RUN}" == "1" ]; then
   cp $ipynb_file $pub_file
 else
   printf "available: $ACCELERATOR\n"
-  accel=$(python .actions/helpers.py valid-accelerator $1 2>&1)
+  accel=$(python .actions/assistant.py valid-accelerator $1 2>&1)
   if [ $accel == 1 ]
   then
     printf "Processing: $ipynb_file\n"
@@ -51,14 +59,18 @@ else
   fi
 fi
 
-python .actions/helpers.py update-env-details $1
+# Export the actual packages used in runtime
+python .actions/assistant.py update-env-details $1
 
+# copy and add to version the enriched meta config
 cp $meta_file $pub_meta_file
 git add $pub_meta_file
 
+# if thumb image is linked to to the notebook, copy and version it too
 if [ ! -z $thumb_file ]; then
   cp $thumb_file $pub_thumb_file
   git add $pub_thumb_file
 fi
 
+# add the generated notebook to version
 git add $pub_file
