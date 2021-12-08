3fc3887b990c509ac0e4a082eb8cd53c19fbac7d
Daniel Dale <danny.dale@gmail.com>
2021-12-08 12:33:03 -0800

    numerous expository enhancements and switch to DeBERTav3

--- lightning_examples/finetuning-scheduler/finetuning-scheduler.py
+++ lightning_examples/finetuning-scheduler/finetuning-scheduler.py
@@ -1,24 +1,20 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.13.2
-# ---
-
 # %% [markdown]
 # ## Scheduled Finetuning
 #
+# Training with the ``FinetuningScheduler`` callback confers a host of benefits. It was created to meet the following
+# objectives in roughly descending order of priority:
+#
+# - maximize finetuning flexibility
+# - expedite and facilitate exploration of model tuning dynamics in research
+# - allow marginal performance improvements of finetuned models
+#
 # <div style="display:inline" id="a1">
 #
-# The ``FinetuningScheduler`` callback enables multi-phase,
-# scheduled finetuning of foundational models. Gradual unfreezing (i.e. thawing) can help maximize
+# Fundamentally, the ``FinetuningScheduler`` callback enables scheduled, multi-phase,
+# finetuning of foundational models. Gradual unfreezing (i.e. thawing) can help maximize
 # foundational model knowledge retention while allowing (typically upper layers of) the model to
 # optimally adapt to new tasks during transfer learning [1, 2, 3](#f1)
+#
 #
 # </div>
 #
@@ -34,10 +30,15 @@
 # ![FinetuningScheduler explicit loss animation](fts_explicit_loss_anim.gif)
 
 # %% [markdown]
+#
 # ## Basic Usage
+#
+# <div id="basic_usage">
 #
 # If no finetuning schedule is provided by the user, ``FinetuningScheduler`` will generate a
 # [default schedule](#The-Default-Finetuning-Schedule) and proceed to finetune according to the generated schedule, using default ``FTSEarlyStopping`` and ``FTSCheckpoint`` callbacks with ``monitor=val_loss``.
+#
+# </div>
 #
 # ```python
 # from pytorch_lightning import Trainer
@@ -49,7 +50,7 @@
 # ## The Default Finetuning Schedule
 #
 # Schedule definition is facilitated via the ``gen_ft_schedule`` method which dumps a default finetuning schedule (by default using a naive, 2-parameters per level heuristic) which can be adjusted as
-# desired by the user and/or subsequently passed to the callback. Using the default/implicitly generated schedule will often be less computationally efficient than a user-defined finetuning schedule but can often serve as a good baseline for subsquent explicit schedule refinement and will marginally outperform many explicit schedules.
+# desired by the user and/or subsequently passed to the callback. Using the default/implicitly generated schedule will likely be less computationally efficient than a user-defined finetuning schedule but is useful for exploring a model's finetuning behavior and can serve as a good baseline for subsquent explicit schedule refinement.
 
 # %% [markdown]
 # ## Specifying a Finetuning Schedule
@@ -147,8 +148,6 @@
 #
 # The following example demonstrates the use of ``FinetuningScheduler`` to finetune a small foundational model on the [RTE](https://huggingface.co/datasets/viewer/?dataset=super_glue&config=rte) task of [SuperGLUE](https://super.gluebenchmark.com/). Iterative early-stopping will be applied according to a user-specified schedule.
 #
-# ``FinetuningScheduler`` can be used to achieve non-trivial model performance improvements in both implicit and explicit scheduling contexts at an also non-trivial computational cost.
-#
 
 # %%
 import os
@@ -166,6 +165,7 @@ from pytorch_lightning.utilities.cli import CALLBACK_REGISTRY, _Registry
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from torch.utils.data import DataLoader
 from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
+from transformers import logging as transformers_logging
 
 # %%
 # a couple helper functions to prepare code to work with a user module registry
@@ -200,12 +200,14 @@ def instantiate_registered_class(init: Dict[str, Any], args: Optional[Union[Any,
     Returns:
         The instantiated class object.
     """
+    class_module, class_name, args_class = None, None, None
+    shortcircuit_local = False
     kwargs = init.get("init_args", {})
+    class_path = init.get("class_path", None)
     if args and not isinstance(args, tuple):
         args = (args,)
-    shortcircuit_local = False if "." in init["class_path"] else True
-    class_module, class_name, args_class = None, None, None
-    if init.get("class_path", None):
+    if class_path:
+        shortcircuit_local = False if "." in class_path else True
         if not shortcircuit_local:
             class_module, class_name = init["class_path"].rsplit(".", 1)
         else:  # class is expected to be locally defined
@@ -236,6 +238,7 @@ AVAIL_GPUS = torch.cuda.device_count()
 TASK_NUM_LABELS = {"boolq": 2, "rte": 2}
 DEFAULT_TASK = "rte"
 
+transformers_logging.set_verbosity_error()
 # ignore warnings related tokenizers_parallelism/DataLoader parallelism tradeoff and
 #  expected logging behavior
 for warnf in [".*does not have many workers*", ".*The number of training samples.*"]:
@@ -262,8 +265,8 @@ class RteBoolqDataModule(pl.LightningDataModule):
         model_name_or_path: str,
         task_name: str = DEFAULT_TASK,
         max_seq_length: int = 128,
-        train_batch_size: int = 32,
-        eval_batch_size: int = 32,
+        train_batch_size: int = 16,
+        eval_batch_size: int = 16,
         tokenizers_parallelism: bool = True,
         **dataloader_kwargs: Any,
     ):
@@ -373,12 +376,21 @@ class RteBoolqModule(pl.LightningModule):
             self.task_name = DEFAULT_TASK
             rank_zero_warn(f"Invalid task_name '{task_name}'. Proceeding with the default task: '{DEFAULT_TASK}'")
         self.num_labels = TASK_NUM_LABELS[self.task_name]
-        self.save_hyperparameters()
         self.experiment_id = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_{experiment_tag}"
         self.model_cfg = model_cfg or {}
         conf = AutoConfig.from_pretrained(model_name_or_path, num_labels=self.num_labels, local_files_only=False)
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, config=conf)
         self.model.config.update(self.model_cfg)  # apply model config overrides
+        self.init_hparams = {
+            "optimizer": self.optimizer_init,
+            "lr_scheduler": self.lr_scheduler_init,
+            "pl_lrs_cfg": self.pl_lrs_cfg,
+            "model_config": self.model.config,
+            "model_name_or_path": model_name_or_path,
+            "task_name": self.task_name,
+            "experiment_id": self.experiment_id,
+        }
+        self.save_hyperparameters(self.init_hparams)
         self.metric = datasets.load_metric("super_glue", self.task_name, experiment_id=self.experiment_id)
         self.no_decay = ["bias", "LayerNorm.weight"]
         self.finetuningscheduler_callback = None
@@ -400,24 +412,14 @@ class RteBoolqModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
         outputs = self(**batch)
         val_loss, logits = outputs[:2]
-
         if self.num_labels >= 1:
             preds = torch.argmax(logits, axis=1)
         elif self.num_labels == 1:
             preds = logits.squeeze()
-
         labels = batch["labels"]
         self.log("val_loss", val_loss, prog_bar=True)
-
-        return {"loss": val_loss, "preds": preds, "labels": labels}
-
-    def validation_epoch_end(self, outputs):
-        preds = torch.cat([x["preds"] for x in outputs]).detach().cpu().numpy()
-        labels = torch.cat([x["labels"] for x in outputs]).detach().cpu().numpy()
-        loss = torch.stack([x["loss"] for x in outputs]).mean()
         metric_dict = self.metric.compute(predictions=preds, references=labels)
         self.log_dict(metric_dict, prog_bar=True)
-        return loss
 
     def _init_param_groups(self) -> List[Dict]:
         """Initialize the parameter groups. Used to ensure weight_decay is not applied to our specified bias
@@ -465,22 +467,46 @@ class RteBoolqModule(pl.LightningModule):
         return super().configure_callbacks()
 
 
+# %% [markdown]
+# ### Our Training Sessions
+#
+# We'll be comparing three different finetuning training configurations. Every configuration in this example depends
+# upon a shared set of defaults, only differing in their respective finetuning schedules.
+#
+# | Experiment Tag    | Training Scenario Description                                          |
+# |:-----------------:| ---------------------------------------------------------------------- |
+# | ``fts_explicit``  | Training with a finetuning schedule explicitly provided by the user    |
+# | ``nofts_baseline``| A baseline finetuning training session (without scheduled finetuning)  |
+# | ``fts_implicit``  | Training with an implicitly generated finetuning schedule (the default)|
+#
+# Let's begin by configuring the ``fts_explicit`` scenario. We'll subsequently run the other two scenarios for
+# comparison.
+
 # %%
 # Let's create a finetuning schedule for our model and run an explicitly scheduled finetuning training scenario with it
-# Please see the documentation for a full description of the schedule format
+# Please see the ``FinetuningScheduler`` documentation for a full description of the schedule format
+
+
 ft_schedule_yaml = """
 0:
-  params: # the parameters for each phase definition can be fully specified
+  params:
   - model.classifier.bias
   - model.classifier.weight
+  - model.pooler.dense.bias
+  - model.pooler.dense.weight
+  - model.deberta.encoder.LayerNorm.bias
+  - model.deberta.encoder.LayerNorm.weight
+  - model.deberta.encoder.rel_embeddings.weight
+  - model.deberta.encoder.layer.{0,11}.(output|attention|intermediate).*
 1:
-  params: # or specified via a regex
-  - model.albert.pooler.*
+  params:
+  - model.deberta.embeddings.LayerNorm.bias
+  - model.deberta.embeddings.LayerNorm.weight
 2:
   params:
-  - model.albert.encoder.*.ffn_output.*
+  - model.deberta.embeddings.word_embeddings.weight
 """
-ft_schedule_name = "RteBoolqModule_ft_schedule_albert_base.yaml"
+ft_schedule_name = "RteBoolqModule_ft_schedule_deberta_base.yaml"
 # Let's write the schedule to a file so we can simulate loading an explicitly defined finetuning
 # schedule.
 with open(ft_schedule_name, "w") as f:
@@ -489,7 +515,7 @@ with open(ft_schedule_name, "w") as f:
 # %%
 datasets.set_progress_bar_enabled(False)
 pl.seed_everything(42)
-dm = RteBoolqDataModule(model_name_or_path="albert-base-v2", tokenizers_parallelism=False)
+dm = RteBoolqDataModule(model_name_or_path="microsoft/deberta-v3-base", tokenizers_parallelism=False)
 
 # %% [markdown]
 # ### Optimizer Configuration
@@ -531,28 +557,40 @@ optimizer_init = {
 # %%
 lr_scheduler_init = {
     "class_path": "torch.optim.lr_scheduler.CosineAnnealingWarmRestarts",
-    # "init_args": {"T_0": 1, "T_mult": 2, "eta_min": 1e-07},
-    "init_args": {"T_0": 1, "T_mult": 2, "eta_min": 0},
+    "init_args": {"T_0": 1, "T_mult": 2, "eta_min": 1e-07},
 }
 pl_lrs_cfg = {"interval": "epoch", "frequency": 1, "name": "CosineAnnealingWarmRestarts"}
 
 # %%
-model = RteBoolqModule(
-    model_name_or_path="albert-base-v2",
-    optimizer_init=optimizer_init,
-    lr_scheduler_init=lr_scheduler_init,
-    pl_lrs_cfg=pl_lrs_cfg,
-    experiment_tag="fts_explicit",
-)
-callbacks = [
-    FinetuningScheduler(ft_schedule=ft_schedule_name, max_depth=2),  # type: ignore # noqa
-    FTSEarlyStopping(monitor="val_loss", min_delta=0.001, patience=2),  # type: ignore # noqa
-    FTSCheckpoint(monitor="val_loss", save_top_k=5),  # type: ignore # noqa
-]
-example_logdir = "lightning_logs"
-logger = TensorBoardLogger(example_logdir, name="fts_explicit")
+# Load our lightning module...
+lightning_module_kwargs = {
+    "model_name_or_path": "microsoft/deberta-v3-base",
+    "optimizer_init": optimizer_init,
+    "lr_scheduler_init": lr_scheduler_init,
+    "pl_lrs_cfg": pl_lrs_cfg,
+}
+model = RteBoolqModule(**lightning_module_kwargs, experiment_tag="fts_explicit")
+
+# %% [markdown]
+# ### Callback Configuration
+#
+# The only callback required to invoke the ``FinetuningScheduler`` is the ``FinetuningScheduler`` callback itself.
+# Default versions of ``FTSCheckpoint`` and ``FTSEarlyStopping`` (if not specifying ``epoch_only_transitions``) will be
+# included ([as discussed above](#basic_usage)) if not provided in the callbacks list. For demonstration purposes I'm
+# including example configurations of all three callbacks below.
 
 # %%
+earlystopping_kwargs = {"monitor": "val_loss", "min_delta": 0.001, "patience": 2}
+checkpoint_kwargs = {"monitor": "val_loss", "save_top_k": 1}
+fts_kwargs = {"max_depth": 1}
+callbacks = [
+    FinetuningScheduler(ft_schedule=ft_schedule_name, **fts_kwargs),  # type: ignore # noqa
+    FTSEarlyStopping(**earlystopping_kwargs),  # type: ignore # noqa
+    FTSCheckpoint(**checkpoint_kwargs),  # type: ignore # noqa
+]
+# %%
+example_logdir = "lightning_logs"
+logger = TensorBoardLogger(example_logdir, name="fts_explicit")
 # optionally start tensorboard and monitor progress graphically while viewing multi-phase finetuning specific training
 # logs in the cell output below by uncommenting the next 3 lines
 # # !mkdir -p $example_logdir
@@ -565,7 +603,7 @@ enable_progress_bar = False
 def train() -> None:
     trainer = pl.Trainer(
         enable_progress_bar=enable_progress_bar,
-        # max_epochs=1,
+        max_epochs=100,
         precision=16,
         accelerator="gpu",
         devices=1,
@@ -580,6 +618,91 @@ if AVAIL_GPUS > 0:
 else:
     print("Given the multiple phases of finetuning demonstrated, this notebook is best used with a GPU")
 
+# %% [markdown]
+# ### Running the Baseline and Implicit Finetuning Scenarios
+#
+# Let's now compare our ``nofts_baseline`` and ``fts_implicit`` scenarios with the ``fts_explicit`` one we just ran.
+# We'll need to update our callbacks list, using the core PL ``EarlyStopping`` and ``ModelCheckpoint`` callbacks for the
+# ``nofts_baseline`` (which operate identically to their FTS analogs apart from the recursive training support).
+# For both core PyTorch Lightning and user-registered callbacks, we can define our callbacks using a dictionary as we do
+# with the LightningCLI. This allows us to avoid managing imports and support more complex configuration separated from
+# code.
+# We'll be using identical callback configurations to the ``fts_explicit`` scenario. Keeping ``max_depth`` for the
+# implicit schedule will limit finetuning to just the last 4 parameters of the model, which is only a small fraction
+# of the parameters you'd want to tune for maximum performance. Since the implicit schedule is quite computationally
+# intensive and most useful for exploring model behavior, leaving ``max_depth`` 1 allows us to demo implicit mode
+# behavior while keeping the computational cost and runtime of this notebook reasonable. To review how a full implicit
+# mode run compares to the ``nofts_baseline`` and ``fts_explicit`` scenarios, please see the the following
+# [tensorboard experiment summary](https://tensorboard.dev/experiment/n7U8XhrzRbmvVzC4SQSpWw/).
+
+
+# %%
+
+# reference the core callbacks using the relevant callback_key
+nofts_callback_cfg = [
+    {"callback_key": "EarlyStopping", "init_args": earlystopping_kwargs},
+    {"callback_key": "ModelCheckpoint", "init_args": checkpoint_kwargs},
+]
+# we can use class_paths, class names or fully qualified names for referencing user-registered callbacks as well
+fts_implicit_cfg = [
+    {"callback_key": "FinetuningScheduler", "init_args": fts_kwargs},  # use the callback_key
+    {"class_path": "FTSEarlyStopping", "init_args": earlystopping_kwargs},  # just the class_name
+    # or the fully qualified name
+    {
+        "class_path": "pytorch_lightning.callbacks.finetuning_scheduler.fts_supporters.FTSCheckpoint",
+        "init_args": checkpoint_kwargs,
+    },
+]
+nofts_callbacks = [instantiate_registered_class(c) for c in nofts_callback_cfg]
+fts_implicit_callbacks = [instantiate_registered_class(c) for c in fts_implicit_cfg]
+scenario_callbacks = {"nofts_baseline": nofts_callbacks, "fts_implicit": fts_implicit_callbacks}
+
+# %%
+if AVAIL_GPUS > 0:
+    for scenario_name, scenario_callbacks in scenario_callbacks.items():
+        model = RteBoolqModule(**lightning_module_kwargs, experiment_tag=scenario_name)
+        logger = TensorBoardLogger(example_logdir, name=scenario_name)
+        callbacks = scenario_callbacks
+        print(f"Beginning training the '{scenario_name}' scenario")
+        train()
+else:
+    print("Given the multiple phases of finetuning demonstrated, this notebook is best used with a GPU")
+
+# %% [markdown]
+# ### Reviewing the Training Results
+#
+# See the [tensorboard experiment summaries](https://tensorboard.dev/experiment/n7U8XhrzRbmvVzC4SQSpWw/) to get a sense
+# of the relative computational and performance tradeoffs associated with these ``FinetuningScheduler`` configurations.
+# The summary compares a full ``fts_implicit`` execution to ``fts_explicit`` and ``nofts_baseline`` scenarios using DDP
+# training with 2 GPUs. The full logs/schedules for all three scenarios are available
+# [here](https://drive.google.com/file/d/1LrUcisRLHeJgh_BDOOD_GUBPp5iHAkoR/view?usp=sharing) and the checkpoints
+# produced in the scenarios [here](https://drive.google.com/file/d/1t7myBgcqcZ9ax_IT9QVk-vFH_l_o5UXB/view?usp=sharing)
+# (caution, ~3.5GB).
+#
+# [![fts_explicit_accuracy](fts_explicit_accuracy.png)](https://tensorboard.dev/experiment/n7U8XhrzRbmvVzC4SQSpWw/#scalars&_smoothingWeight=0&runSelectionState=eyJmdHNfZXhwbGljaXQiOnRydWUsIm5vZnRzX2Jhc2VsaW5lIjpmYWxzZSwiZnRzX2ltcGxpY2l0IjpmYWxzZX0%3D)
+# [![nofts_baseline](nofts_baseline_accuracy.png)](https://tensorboard.dev/experiment/n7U8XhrzRbmvVzC4SQSpWw/#scalars&_smoothingWeight=0&runSelectionState=eyJmdHNfZXhwbGljaXQiOmZhbHNlLCJub2Z0c19iYXNlbGluZSI6dHJ1ZSwiZnRzX2ltcGxpY2l0IjpmYWxzZX0%3D)
+#
+# Note there could be around ~1% variation in performance from the tensorboard summaries generated by this notebook
+# which uses DDP_SPAWN and 1 GPU.
+#
+# ``FinetuningScheduler`` expands the space of possible finetuning schedules and the composition of more sophisticated schedules can
+# yield mariginal finetuning performance gains. That stated, it should be emphasized the primary utility of ``FinetuningScheduler`` is to grant
+# greater finetuning flexibility for model exploration in research. For example, glancing at DeBERTav3's implicit training
+# run, a critical tuning transition point is immediately apparent:
+#
+# [![implicit_training_transition](implicit_training_transition.png)](https://tensorboard.dev/experiment/n7U8XhrzRbmvVzC4SQSpWw/#scalars&_smoothingWeight=0&runSelectionState=eyJmdHNfZXhwbGljaXQiOmZhbHNlLCJub2Z0c19iYXNlbGluZSI6ZmFsc2UsImZ0c19pbXBsaWNpdCI6dHJ1ZX0%3D)
+#
+# Our val_loss begins a precipitous decline at step 3119 which corresponds to phase 17 in the schedule. Referring to our
+# schedule, in phase 17 we're beginning tuning the attention parameters of our 10th encoder layer (of 11). Interesting!
+# Though beyond the scope of this tutorial, it might be worth investigating these dynamics further and
+# ``FinetuningScheduler`` allows one to do just that quite easily.
+#
+# %% [markdown]
+#
+# Note that though this example is intended to capture a common usage scenario, substantial variation is expected
+# among use cases and models.
+# In summary, ``FinetuningScheduler`` provides increased finetuning flexibility that can be useful in a variety of
+# contexts from exploring model tuning behavior to maximizing performance.
 # %% [markdown]
 # ## Footnotes
 #
