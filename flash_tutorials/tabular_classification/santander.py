e552eed8bfed8183bd20a34fdf7e16ee1fbc92ba
PabloAMC <pmorenocf@alumnos.unex.es>
2021-12-18 01:32:02 +0100

    Better explained

--- flash_tutorials/tabular_classification/santander.py
+++ flash_tutorials/tabular_classification/santander.py
@@ -22,13 +22,20 @@ from flash.core.classification import LabelsOutput
 from flash.core.data.utils import download_data
 from flash.tabular.classification import TabularClassificationData, TabularClassifier
 from imblearn.over_sampling import SMOTE
-from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+from pytorch_lightning.utilities.seed import seed_everything
 
 DATASET_PATH = os.environ.get("PATH_DATASETS", "data/")
 
 # %%
+
+seed_everything(seed=42)
+
 # todo: to be changed
 download_data("https://pl-flash-data.s3.amazonaws.com/kaggle_electricity.zip", DATASET_PATH)
+
+# %% [markdown]
+# ## Loading and inspecting the data
 
 # %%
 # ## Download the data
@@ -38,49 +45,91 @@ df_predict = pd.read_csv(f"{DATASET_PATH}/test.csv")
 df_train = df_train.drop("ID_code", axis=1)
 
 # %%
-
 # Some data inspection
+print(df_train.shape)
+print(df_predict.shape)
+
+df_train.describe()
+
+# We check that there is no missing data in the training (nor test) set
+df_train.isnull().values.any()
+
+# %% [markdown]
+# ## Data preprocessing
+# We first do two things:
+# 1. Divide the training data into a training, validation and test sets.
+# 2. Resample the training data to have the same number of samples for each class, as the original training data is unbalanced.
+
+# %%
+
+train, rem = train_test_split(df_train, test_size=0.2)
+validation, test = train_test_split(rem, test_size=0.5)
 
 # %%
 
 sm = SMOTE(sampling_strategy="auto", random_state=42)
 
-oversampled_X, oversampled_Y = sm.fit_resample(df_train.drop("target", axis=1), df_train["target"])
+oversampled_X, oversampled_Y = sm.fit_resample(train.drop("target", axis=1), train["target"])
 df_upsampled = pd.concat([pd.DataFrame(oversampled_Y), pd.DataFrame(oversampled_X)], axis=1)
-# %%
 
-# df_upsampled can be substituted by df_train
+# %% [markdown]
+# ## Flash TabularClassificationData DataModule
+# The objective now is to create a datamodule from TabularClassificationData that can be used with Flash.
+# %%
 
 datamodule = TabularClassificationData.from_data_frame(
     numerical_fields=["var_" + str(i) for i in range(200)],
     target_fields="target",
-    train_data_frame=df_upsampled,
+    train_data_frame = df_upsampled,
+    val_data_frame = validation,
+    test_data_frame = test,
     predict_data_frame=df_predict,
-    batch_size=256,
+    batch_size = 256
 )
 
+# %% [markdown]
+# ## Flash TabularClassifier Model.
+# We indicate the torchmetrics.AUROC metric to be used for evaluating the model, an LR scheduler, and the optimizer.
 # %%
-
-
 # It is important that one uses the modular, not the function form of the metric. That is, do not use torchmetrics.functional.auroc()
 
-model = TabularClassifier.from_data(datamodule, metrics=[torchmetrics.AUROC(num_classes=datamodule.num_classes)])
+model = TabularClassifier.from_data(datamodule, 
+                                    metrics=[torchmetrics.AUROC(num_classes=datamodule.num_classes)], 
+                                    lr_scheduler=("ExponentialLR", {"gamma": 0.95}),
+                                    optimizer = 'adamw')
 model.output = LabelsOutput()
+
+# %% [markdown]
+# ## Trainer
 
 # %%
 
-trainer = Trainer(gpus=torch.cuda.device_count())
+trainer = Trainer(max_epochs = 10, gpus=torch.cuda.device_count())
 
 res = trainer.tuner.lr_find(model, datamodule=datamodule, min_lr=1e-5)
 print(f"Suggested learning rate: {res.suggestion()}")
 res.plot(show=True, suggest=True).show()
 
-model.learning_rate = res.suggestion()
+# Automatic learning rate suggestion seems unstable, so we'll use a fixed learning rate
+model.learning_rate = 5e-3 #res.suggestion()
 
+# %% [markdown]
+# ## Training
+# Finally, we train the model for a few epochs and evaluate it on the test set.
+
+# %%
 trainer.fit(model, datamodule=datamodule)
 
 # %%
+trainer.validate(model, datamodule=datamodule)
 
+# %%
+trainer.test(model, datamodule = datamodule)
+
+# %% [markdown]
+# ## Predictions
+# We can now predict the test set and submit it to Kaggle.
+# %%
 predictions = trainer.predict(model, datamodule=datamodule)
 
 # %%
@@ -96,9 +145,9 @@ predict_datamodule = TabularClassificationData.from_data_frame(
 predictions = trainer.predict(model, datamodule=predict_datamodule)
 
 # %%
-# todo: warning, the order of the predictions might not be ordered!
-id_code = ["test_" + str(i) for i in range(len(predictions[0]))]
-data = {"ID_code": id_code, "target": predictions[0]}
-pred_df = pd.DataFrame(data)
 
-pred_df.to_csv("submission.csv", index=False)
+df_predict["target"] = predictions[0]
+id_code = ['test_'+str(i) for i in range(len(predictions[0]))]
+df_predict['ID_code'] = id_code
+
+df_predict.to_csv('submission.csv',columns = ['ID_code','target'], index=False)
