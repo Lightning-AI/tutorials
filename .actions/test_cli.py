226bb9750d40d5aee826e5f47dcdf5d2d1e02854
Jirka <jirka.borovec@seznam.cz>
2022-02-07 21:48:07 +0100

    CI: add more inter. testing
    * use caching

--- .actions/test_cli.py
+++ .actions/test_cli.py
@@ -9,6 +9,10 @@ _PATH_DIR_SIMPLE = os.path.join(_PATH_TEMPLATES, "simple")
 _PATH_DIR_TITANIC = os.path.join(_PATH_TEMPLATES, "titanic")
 
 
+def _path_in_dir(fname: str, folder: str = _PATH_ROOT) -> str:
+    return os.path.join(folder, fname)
+
+
 @pytest.mark.parametrize(
     "cmd,args",
     [
@@ -16,7 +20,7 @@ _PATH_DIR_TITANIC = os.path.join(_PATH_TEMPLATES, "titanic")
         ("list_dirs", [".", ".ipynb"]),
         ("bash_render", [_PATH_DIR_SIMPLE]),
         ("bash_test", [_PATH_DIR_SIMPLE]),
-        # ("group_folders", [_PATH_SIMPLE]),
+        ("group_folders", [_path_in_dir("master-diff.txt"), _path_in_dir("dirs-b1.txt"), _path_in_dir("dirs-b2.txt")]),
         ("augment_script", [_PATH_DIR_SIMPLE]),
         ("copy_notebooks", [_PATH_ROOT]),
         ("update_env_details", [_PATH_DIR_SIMPLE]),
