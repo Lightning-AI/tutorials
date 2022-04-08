835d15ee94467c02cb8e49991d02e428e9885c21
Daniel Dale <danny.dale@gmail.com>
2022-04-07 17:54:39 -0700

    require only finetuning-scheduler dependency now that it is available on pypi

--- lightning_examples/finetuning-scheduler/finetuning-scheduler.py
+++ lightning_examples/finetuning-scheduler/finetuning-scheduler.py
@@ -1,15 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.13.7
-# ---
-
 # %% [markdown]
 # ## Scheduled Finetuning
 #
