8a836dfa5046c981b73735982a58e5ddf9000aab
Jirka <jirka.borovec@seznam.cz>
2022-04-15 05:07:15 +0200

    skip pip

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -362,8 +362,9 @@ class AssistantCLI:
             "pip --version",
         ]
         # and install specific packages
-        pip_req, pip_args = AssistantCLI._parse_requirements(folder)
-        cmd += [f"pip install {pip_req} {pip_args}", "pip list"]
+        if AssistantCLI._valid_accelerator(folder):
+            pip_req, pip_args = AssistantCLI._parse_requirements(folder)
+            cmd += [f"pip install {pip_req} {pip_args}", "pip list"]
         # Export the actual packages used in runtime
         cmd.append(f"meta_file=$(python .actions/assistant.py update-env-details {folder} --base_path .)")
         # show created meta config
