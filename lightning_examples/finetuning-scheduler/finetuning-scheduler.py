bed34ac71b1a7fb1fd7ffc29022cb011be2af754
Daniel Dale <danny.dale@gmail.com>
2022-08-06 18:49:26 -0700

    remove jupytext metadata

--- lightning_examples/finetuning-scheduler/finetuning-scheduler.py
+++ lightning_examples/finetuning-scheduler/finetuning-scheduler.py
@@ -1,15 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.14.1
-# ---
-
 # %% [markdown]
 # ## Scheduled Fine-Tuning with the Fine-Tuning Scheduler Extension
 #
