5e41e2f39ba70d5b5cb7de3f1ebdfd7d7c7b8699
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-08-26 15:05:48 +0530

    Fix formatting for markdown

--- flash_tutorials/text_classification/text_classification.py
+++ flash_tutorials/text_classification/text_classification.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# <a href="https://colab.research.google.com/github/PyTorchLightning/lightning-flash/blob/master/flash_notebooks/text_classification.ipynb" target="_parent">
-#     <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
-# </a>
-
+# %% [markdown]
 # In this notebook, we'll go over the basics of lightning Flash by finetunig a TextClassifier on [IMDB Dataset](https://www.imdb.com/interfaces/).
 # 
 # # Finetuning
@@ -22,47 +19,26 @@
 #  
 # 
 # - 4. Train the target model on a target dataset, such as Hymenoptera Dataset with ants and bees. However, freezing some layers at training start such as the backbone tends to be more stable. In Flash, it can easily be done with `trainer.finetune(..., strategy="freeze")`. It is also common to `freeze/unfreeze` the backbone. In `Flash`, it can be done with `trainer.finetune(..., strategy="freeze_unfreeze")`. If a one wants more control on the unfreeze flow, Flash supports `trainer.finetune(..., strategy=MyFinetuningStrategy())` where `MyFinetuningStrategy` is subclassing `pytorch_lightning.callbacks.BaseFinetuning`.
-# 
-# ---
-#   - Give us a ⭐ [on Github](https://www.github.com/PytorchLightning/pytorch-lightning/)
-#   - Check out [Flash documentation](https://lightning-flash.readthedocs.io/en/latest/)
-#   - Check out [Lightning documentation](https://pytorch-lightning.readthedocs.io/en/latest/)
-#   - Join us [on Slack](https://www.pytorchlightning.ai/community)
-
-# ### Setup  
-# Lightning Flash is easy to install. Simply ```pip install lightning-flash```
-
-# In[ ]:
-
-
-get_ipython().run_cell_magic('capture', '', "! pip install 'git+https://github.com/PyTorchLightning/lightning-flash.git#egg=lightning-flash[text]'\n")
-
-
-# In[ ]:
-
+# %%
 
 import flash
 from flash.core.data.utils import download_data
 from flash.text import TextClassificationData, TextClassifier
 
 
+# %% [markdown]
 # ###  1. Download the data
 # The data are downloaded from a URL, and save in a 'data' directory.
-
-# In[ ]:
-
-
+# %%
 download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", 'data/')
 
 
-# <h2>2. Load the data</h2>
+# %% [markdown]
+# ## 2. Load the data</h2>
 # 
 # Flash Tasks have built-in DataModules that you can use to organize your data. Pass in a train, validation and test folders and Flash will take care of the rest.
 # Creates a TextClassificationData object from csv file.
-
-# In[ ]:
-
-
+# %%
 datamodule = TextClassificationData.from_csv(
     "review",
     "sentiment",
@@ -73,67 +49,52 @@ datamodule = TextClassificationData.from_csv(
 )
 
 
-# ###  3. Build the model
+# %% [markdown]
+# ##  3. Build the model
 # 
 # Create the TextClassifier task. By default, the TextClassifier task uses a [tiny-bert](https://huggingface.co/prajjwal1/bert-tiny) backbone to train or finetune your model demo. You could use any models from [transformers - Text Classification](https://huggingface.co/models?filter=text-classification,pytorch)
 # 
 # Backbone can easily be changed with such as `TextClassifier(backbone='bert-tiny-mnli')`
-
-# In[ ]:
-
-
+# %%
 model = TextClassifier(num_classes=datamodule.num_classes, backbone="prajjwal1/bert-tiny")
 
 
-# ###  4. Create the trainer. Run once on data
-
-# In[ ]:
-
-
+# %% [markdown]
+# ##  4. Create the trainer. Run once on data
+# %%
 trainer = flash.Trainer(max_epochs=1)
 
 
-# ###  5. Fine-tune the model
+# %% [markdown]
+# ##  5. Fine-tune the model
 # 
 # The backbone won't be freezed and the entire model will be finetuned on the imdb dataset 
-
-# In[ ]:
-
-
+# %%
 trainer.finetune(model, datamodule=datamodule, strategy="freeze")
 
 
-# ###  6. Test model
-
-# In[ ]:
-
-
+# %% [markdown]
+# ##  6. Test model
+# %%
 trainer.test(model, datamodule=datamodule)
 
 
-# ###  7. Save it!
-
-# In[ ]:
-
-
+# %% [markdown]
+# ##  7. Save it!
+# %%
 trainer.save_checkpoint("text_classification_model.pt")
 
 
-# # Predicting
-
+# %% [markdown]
+# ## 8. Predicting
 # ### 1. Load the model from a checkpoint
-
-# In[ ]:
-
-
+# %%
 model = TextClassifier.load_from_checkpoint("text_classification_model.pt")
 
 
+# %% [markdown]
 # ### 2. Classify a few sentences! How was the movie?
-
-# In[ ]:
-
-
+# %%
 datamodule = TextClassificationData.from_lists(
     predict_data=[
         "Turgid dialogue, feeble characterization - Harvey Keitel a judge?.",
@@ -144,39 +105,3 @@ datamodule = TextClassificationData.from_lists(
 )
 predictions = trainer.predict(model, datamodule=datamodule)
 print(predictions)
-
-
-# <code style="color:#792ee5;">
-#     <h1> <strong> Congratulations - Time to Join the Community! </strong>  </h1>
-# </code>
-# 
-# Congratulations on completing this notebook tutorial! If you enjoyed it and would like to join the Lightning movement, you can do so in the following ways!
-# 
-# ### Help us build Flash by adding support for new data-types and new tasks.
-# Flash aims at becoming the first task hub, so anyone can get started to great amazing application using deep learning. 
-# If you are interested, please open a PR with your contributions !!! 
-# 
-# 
-# ### Star [Lightning](https://github.com/PyTorchLightning/pytorch-lightning) on GitHub
-# The easiest way to help our community is just by starring the GitHub repos! This helps raise awareness of the cool tools we're building.
-# 
-# * Please, star [Lightning](https://github.com/PyTorchLightning/pytorch-lightning)
-# 
-# ### Join our [Slack](https://www.pytorchlightning.ai/community)!
-# The best way to keep up to date on the latest advancements is to join our community! Make sure to introduce yourself and share your interests in `#general` channel
-# 
-# ### Interested by SOTA AI models ! Check out [Bolt](https://github.com/PyTorchLightning/lightning-bolts)
-# Bolts has a collection of state-of-the-art models, all implemented in [Lightning](https://github.com/PyTorchLightning/pytorch-lightning) and can be easily integrated within your own projects.
-# 
-# * Please, star [Bolt](https://github.com/PyTorchLightning/lightning-bolts)
-# 
-# ### Contributions !
-# The best way to contribute to our community is to become a code contributor! At any time you can go to [Lightning](https://github.com/PyTorchLightning/pytorch-lightning) or [Bolt](https://github.com/PyTorchLightning/lightning-bolts) GitHub Issues page and filter for "good first issue". 
-# 
-# * [Lightning good first issue](https://github.com/PyTorchLightning/pytorch-lightning/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22)
-# * [Bolt good first issue](https://github.com/PyTorchLightning/lightning-bolts/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22)
-# * You can also contribute your own notebooks with useful examples !
-# 
-# ### Great thanks from the entire Pytorch Lightning Team for your interest !
-# 
-# <img src="https://raw.githubusercontent.com/PyTorchLightning/lightning-flash/18c591747e40a0ad862d4f82943d209b8cc25358/docs/source/_static/images/logo.svg" width="800" height="200" />
