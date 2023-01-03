26f0edd48825a80ccd3ad1ec8e152521fd2bf39f
Jirka <jirka.borovec@seznam.cz>
2023-01-03 17:46:21 +0100

    short

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -545,9 +545,7 @@ class AssistantCLI:
             folders = [ln.strip() for ln in fp.readlines()]
         # set default so the matrix has at least one runner
         if not folders:
-            return json.dumps(
-                {"free-pass": {"notebook": "", "agent-pool": "azure-cpus", "docker-image": "ubuntu:latest"}}
-            )
+            return json.dumps({"free-pass": {"notebook": "", "agent-pool": "", "docker-image": ""}})
         mtx = {}
         for ln in folders:
             mtx[ln] = {
