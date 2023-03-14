a871081750d12d0a3bfc2bd8ed08bcfcd47ebf95
awaelchli <aedu.waelchli@gmail.com>
2023-03-14 11:58:13 +0100

    update

--- lightning_examples/cifar10-baseline/baseline.py
+++ lightning_examples/cifar10-baseline/baseline.py
@@ -25,7 +25,7 @@ L.seed_everything(7)
 
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 BATCH_SIZE = 256 if torch.cuda.is_available() else 64
-NUM_WORKERS = 0 # int(os.cpu_count() / 2)
+NUM_WORKERS = int(os.cpu_count() / 2)
 
 # %% [markdown]
 # ### CIFAR10 DataLoaders
