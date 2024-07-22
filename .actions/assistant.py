2b8436c80fda1c51dd57b23d003eb72150235c89
jirka <jirka.borovec@seznam.cz>
2024-07-22 20:32:25 +0200

    docker

--- .actions/assistant.py
+++ .actions/assistant.py
@@ -193,7 +193,7 @@ class AssistantCLI:
     _EXT_ARCHIVE_TAR = (".tar", ".gz")
     _EXT_ARCHIVE = _EXT_ARCHIVE_ZIP + _EXT_ARCHIVE_TAR
     _AZURE_POOL = "lit-rtx-3090"
-    _AZURE_DOCKER = "pytorchlightning/tutorials:latest"
+    _AZURE_DOCKER = "pytorchlightning/tutorials:cuda"
 
     @staticmethod
     def _find_meta(folder: str) -> str:
