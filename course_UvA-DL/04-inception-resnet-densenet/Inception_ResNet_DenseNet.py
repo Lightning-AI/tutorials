d39e60f7a11b5e2a1d2f8512176fe3b1ebad1da4
jirka <jirka.borovec@seznam.cz>
2024-07-23 10:30:24 +0200

    pl

--- course_UvA-DL/04-inception-resnet-densenet/Inception_ResNet_DenseNet.py
+++ course_UvA-DL/04-inception-resnet-densenet/Inception_ResNet_DenseNet.py
@@ -295,7 +295,7 @@ class CIFARModule(L.LightningModule):
 
 # %%
 # Callbacks
-from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint  # noqa: E402
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint  # noqa: E402
 
 # %% [markdown]
 # To allow running multiple different models with the same Lightning module, we define a function below that maps a model name to the model class.
